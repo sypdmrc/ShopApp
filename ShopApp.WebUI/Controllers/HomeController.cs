e91bfc77866dfe16b6791dd173bdc398b10ab628
Şuayip DEMİRCİ <suayip.demirci53@gmail.com>
2022-07-30 13:36:19 +0300

    Add project files.

---
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    //localhost:5000/home
+    public class HomeController:Controller
+    {
+        //localhost:5000/home/index
+        public string Index()
+        {
+            return "home/index";
+        }
+
+        //localhost:5000/home/about
+        public string About()
+        {
+            return "home/about";
+        }
+    }
+}
---
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ProductController:Controller
+    {
+        //localhost:5000/product/list
+        public string List()
+        {
+            return "product/list";
+        }
+
+        //localhost:5000/product/details
+        public string Details()
+        {
+            return "product/details";
+        }
+    }
+}
