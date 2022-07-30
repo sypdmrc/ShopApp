31394c7e42df0ede34ade234c4427953bc907666
Şuayip DEMİRCİ <suayip.demirci53@gmail.com>
2022-07-30 17:19:24 +0300

    Ürün listesi ve kategori oluşturulması ve ProductViewModel içerisine tanımlanması ve sonunda View'e gönderilmesi

--- ShopApp.WebUI/Controllers/ProductController.cs
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShopApp.WebUI.Models;
+using ShopApp.WebUI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +8,16 @@ using System.Threading.Tasks;
 
 namespace ShopApp.WebUI.Controllers
 {
-    public class ProductController:Controller
+    public class ProductController : Controller
     {
         //localhost:5000/product/index
         public IActionResult Index()
         {
-            Product product = new Product 
-            { 
-                Name="Iphone 13",
-                Price=15000,
-                Description="Güzel Telefon"
+            Product product = new Product
+            {
+                Name = "Iphone 13",
+                Price = 15000,
+                Description = "Güzel Telefon"
             };
 
             //ViewData["Product"] = product;
@@ -31,7 +32,35 @@ namespace ShopApp.WebUI.Controllers
         //localhost:5000/product/list
         public IActionResult List()
         {
-            return View();
+            var products = new List<Product>()
+            {
+                new Product{Name="Iphone 8",Price=3000,Description="İyi Telefon"},
+                new Product{Name="Iphone 5",Price=2000,Description="Fena Değil"},
+                new Product{Name="Iphone 7",Price=3000,Description="İyi Telefon"},
+                new Product{Name="Iphone 6",Price=4000,Description="Fena Değil"},
+                new Product{Name="Iphone 9",Price=9000,Description="İyi Telefon"},
+                new Product{Name="Iphone 10",Price=12000,Description="Fena Değil"},
+                new Product{Name="Iphone 8+",Price=13000,Description="İyi Telefon"},
+                new Product{Name="Iphone 11",Price=12500,Description="Fena Değil"},
+                new Product{Name="Iphone 12",Price=13500,Description="İyi Telefon"},
+                new Product{Name="Iphone 13",Price=18000,Description="Fena Değil"},
+                new Product{Name="Iphone X",Price=30000,Description="İyi Telefon"},
+                new Product{Name="Iphone 5+",Price=2800,Description="Fena Değil"},
+            };
+
+            var category = new Category
+            {
+                Name = "Telefonlar",
+                Description = "Telefon Kategorisi"
+            };
+
+            var productViewModel = new ProductViewModel()
+            {
+                Category = category,
+                Products = products
+            };
+
+            return View(productViewModel);
         }
 
         //localhost:5000/product/details
