991c4d5d75d3ba667396f7fd8be22ceb01aacf2c
Şuayip DEMİRCİ <suayip.demirci53@gmail.com>
2022-07-31 21:36:08 +0300

    Css,image,js sayfalarının eklenmesi ve startup.cs'e UseStaticFiles eklenmesi

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -22,6 +22,8 @@ namespace ShopApp.WebUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();//wwwroot altýndakiler çalýþýr
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
